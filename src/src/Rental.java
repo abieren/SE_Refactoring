51460205d4f0baf9eefea876bac8718d4b53bb0b
Alexander Bierenstiel <bierenstiel.alexander@gmail.com>
2017-05-09 15:35:27 +0200

    refactor: changed method to take class memebers for calculation

--- src/src/Rental.java
+++ src/src/Rental.java
@@ -13,21 +13,21 @@ class Rental {
     public Movie getMovie() {
         return movie;
     }
-    public double getCharge(Rental aRental) {
+    public double getCharge() {
         double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
+        switch (this.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
+                if (this.getDaysRented() > 2)
+                    result += (this.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
+                result += this.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
+                if (this.getDaysRented() > 3)
+                    result += (this.getDaysRented() - 3) * 1.5;
                 break;
         }
         return result;
